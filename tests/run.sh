0927a6442c84c9b594f6bb08671e27b0ac1b969d
Carlos Medina Sanchez <carlos.medina.sanchez@bbva.com>
2017-08-04 14:56:13 +0200

    feat(tests): modify docker-compose to be able to mock with production data

--- tests/run.sh
+++ tests/run.sh
@@ -2,9 +2,32 @@
 
 set -e
 
-trap 'docker-compose -p ${BUILD_TAG} down --volumes' EXIT
+volume_token='{{ volume }}'
+volume=".:/temp"
+
+entrypoint_token='{{ entrypoint }}'
+entrypoint="/usr/bin/env bash -c 'until mongo dashboarddb --host mongo < data/main.js; do >&2 echo Mongo is unavailable - sleeping & sleep 1; done'"
+
+trap 'docker-compose -p ${BUILD_TAG} down --volumes; rm -f docker-compose.yml' EXIT
 
 if [ -z ${BUILD_TAG+x} ]; then export BUILD_TAG=tests; fi
+
+if [ -n "$MONGODUMP_BUCKET" ];
+then
+    dumps=(`aws s3 --profile $AWS_PROFILE --region $AWS_REGION ls $MONGODUMP_BUCKET/ | awk '{print $4}'`)
+    last_dump=${dumps[-1]}
+    aws s3 --profile $AWS_PROFILE --region $AWS_REGION cp $MONGODUMP_BUCKET/${last_dump} /tmp
+    tar -xf /tmp/${last_dump} --directory /tmp
+
+    entrypoint="/usr/bin/env bash -c 'until mongorestore --host mongo --db dashboarddb --drop /temp/dashboarddb; do >&2 echo Mongo is unavailable - sleeping & sleep 1; done'"
+    volume="/tmp/${last_dump%%.*}:/temp"
+
+    awk -v old="${volume_token}" -v new="${volume}" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' < docker-compose-template.yml > docker-compose-template.tmp
+    awk -v old="${entrypoint_token}" -v new="${entrypoint}" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' < docker-compose-template.tmp > docker-compose.yml && rm docker-compose-template.tmp
+fi
+
+awk -v old="${volume_token}" -v new="${volume}" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' < docker-compose-template.yml > docker-compose-template.tmp
+awk -v old="${entrypoint_token}" -v new="${entrypoint}" 's=index($0,old){$0=substr($0,1,s-1) new substr($0,s+length(old))} 1' < docker-compose-template.tmp > docker-compose.yml && rm docker-compose-template.tmp
 
 docker-compose -p ${BUILD_TAG} run -u $(id -u) mongo-populate
 docker-compose -p ${BUILD_TAG} run -u $(id -u) --service-ports app
