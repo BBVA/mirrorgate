9141e8c632b0ea45e671e4e50b2212c084eeac78
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-08-14 15:09:37 +0200

    fix(websocket): security config

--- mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/config/RestConfig.java
+++ mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/config/RestConfig.java
@@ -53,14 +53,14 @@ public class RestConfig {
                         .disable()
                     .authorizeRequests()
                         .antMatchers("/health").permitAll()
+                        .antMatchers("/websocket").permitAll()
                         .antMatchers(HttpMethod.OPTIONS,"**").permitAll()
                         .antMatchers(HttpMethod.POST, "/api/**").hasAuthority(SecurityAuthoritiesEnum.COLLECTOR.toString())
                         .antMatchers(HttpMethod.DELETE, "/api/**").hasAuthority(SecurityAuthoritiesEnum.COLLECTOR.toString())
                         .antMatchers(HttpMethod.GET, "/dashboards/**").hasAnyAuthority(SecurityAuthoritiesEnum.REGULAR.toString(), SecurityAuthoritiesEnum.SCREEN.toString())
                         .antMatchers(HttpMethod.POST, "/dashboards/**").hasAuthority(SecurityAuthoritiesEnum.REGULAR.toString())
                         .antMatchers(HttpMethod.DELETE, "/dashboards/**").hasAuthority(SecurityAuthoritiesEnum.REGULAR.toString())
-                        .antMatchers(HttpMethod.PUT, "/dashboards/**").hasAuthority(SecurityAuthoritiesEnum.REGULAR.toString())
-                        .antMatchers(HttpMethod.GET, "/websocket").hasAnyAuthority(SecurityAuthoritiesEnum.REGULAR.toString(), SecurityAuthoritiesEnum.SCREEN.toString());
+                        .antMatchers(HttpMethod.PUT, "/dashboards/**").hasAuthority(SecurityAuthoritiesEnum.REGULAR.toString());
         }
 
         @Override
