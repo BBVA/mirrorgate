a8516a2714652a8d47a5e6c07c17dcd198267f72
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-08-16 11:28:43 +0200

    fix(websocket): allow any origin

--- mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/config/WebSocketConfig.java
+++ mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
@@ -15,6 +16,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     private SocketHandler socketHandler;
 
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(socketHandler, "/websocket");
+        registry.addHandler(socketHandler, "/websocket")
+            .addInterceptors(new HttpSessionHandshakeInterceptor()).setAllowedOrigins("*");
     }
 }
