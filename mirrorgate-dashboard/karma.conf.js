40e0d05cf0fbff60499328aca3f1a700ede0f200
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-08-17 15:11:46 +0200

    fix(websocket): tests

--- mirrorgate-dashboard/karma.conf.js
+++ mirrorgate-dashboard/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/rivets/dist/rivets.bundled.min.js',
       'bower_components/moment/min/moment.min.js',
       'bower_components/moment-weekday-calc/build/moment-weekday-calc.min.js',
+      'bower_components/sockjs-client/dist/sockjs.min.js',
       'src/js/core/utils.js',
       'src/js/components/Tile.js',
       'src/js/core/Event.js',
