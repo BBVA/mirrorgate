c45c57d2afdbb79fe28b8768d8d3a2c3b9f55ba6
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-10-06 15:48:53 +0200

    feat(events): manage events on front

--- mirrorgate-dashboard/src/js/core/Service.js
+++ mirrorgate-dashboard/src/js/core/Service.js
@@ -98,8 +98,8 @@ var Service = (function() {
     types: {
       builds: new ServiceType(Timer.eventually, 'builds','BuildType'),
       bugs: new ServiceType(Timer.eventually, 'bugs'),
-      stories: new ServiceType(Timer.eventually, 'stories', 'FeatureType'),
-      apps: new ServiceType(Timer.eventually, 'applications'),
+      stories: new ServiceType(Timer.rarely, 'stories', 'FeatureType'),
+      apps: new ServiceType(Timer.eventually, 'applications', 'ReviewType'),
       dashboard: new ServiceType(Timer.never, 'details'),
       dashboards: new ServiceType(Timer.never),
       programincrement: new ServiceType(Timer.eventually, 'programincrement', 'FeatureType'),
