c93594d8633d750b50dbe9748c58a5cc27bd7efc
Carlos Medina Sanchez <carlos.medina.sanchez@bbva.com>
2017-10-25 16:24:06 +0200

    chore(api): log referer when collect feedback

--- mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/security/HeaderSecurityFilter.java
+++ mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/security/HeaderSecurityFilter.java
@@ -34,6 +34,7 @@ public class HeaderSecurityFilter extends OncePerRequestFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(HeaderSecurityFilter.class.getName());
     private static final String USER_HEADER = "X-Forwarded-User";
+    private static final String REFERER_HEADER = "Referer";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -52,6 +53,12 @@ public class HeaderSecurityFilter extends OncePerRequestFilter {
         String xForwardedUser = request.getHeader(USER_HEADER);
 
         LOG.info("X-Forwarded-User header value {}", xForwardedUser);
+
+        String referer = request.getHeader(REFERER_HEADER);
+
+        if(request.getMethod().equals("POST") && request.getRequestURI().contains("/mirrorgate/api/reviews/")) {
+            LOG.info("Referer header value {}", referer);
+        }
 
         Authentication auth = TokenCreator.createHeaderBasedToken(xForwardedUser);
         SecurityContextHolder.getContext().setAuthentication(auth);
