9bcf37b93c8bfc549ab4399c0e1cf98ae7b51107
Enrique Daimiel <enrique.daimiel@bbva.com>
2017-11-07 13:32:09 +0100

    feat(notifications): notify new Slack messages by changing favicon for a short period of time (1 minute)
    Closes #DOST05-466

--- mirrorgate-dashboard/src/js/core/utils.js
+++ mirrorgate-dashboard/src/js/core/utils.js
@@ -14,136 +14,153 @@
  * limitations under the License.
  */
 
-var Utils = {
-  getDashboardId: function() {
-    var dashboard = document.location.hash;
-    if (dashboard && dashboard.length > 1) {
-      dashboard = dashboard.substring(1);
-    } else {
-      var searchParams = new URLSearchParams(window.location.search);
-      dashboard = searchParams.get('board');
-    }
-    return dashboard || '';
-  },
+var Utils = (function() {
+  'use strict';
 
-  getSkin: function () {
-    return this._skin || 'classic';
-  },
+  var breakFavIconTimeout;
 
-  initSkin: function () {
-    var self = this;
-    return new Promise(function (resolve) {
-      Service
-        .get(Service.types.dashboard, Utils.getDashboardId())
-        .addListener(function(details) {
-          details = details && JSON.parse(details);
-          self._skin = (details && details.skin) || 'classic';
-          resolve(self._skin);
-        });
-    });
-  },
-
-  getWorkingDays: function(start, end) {
-    var diff = 0;
-    start = moment(start);
-    end = moment(end);
-    if (start.isBefore(end)) {
-      end.add(-1, 'days');
-      diff = moment().isoWeekdayCalc({
-        rangeStart: start,
-        rangeEnd: end,
-        weekdays: [1, 2, 3, 4, 5],
-        exclusions: [],
-        inclusions: []
-      });
-    }
-    return diff;
-  },
-
-
-  raiseEvent: function(source, detail) {
-    source.dispatchEvent(
-        new CustomEvent('dashboard-updated', {bubbles: true, detail: detail}));
-  },
-
-  toClassName: function(s) { return (s || s === 0) && ('' + s).toLowerCase().replace(' ', '-').replace('_','-'); },
-
-  openDashboard : function(type, boardId) {
-    document.location.href = type + '.html?board=' + encodeURIComponent( boardId || Utils.getDashboardId());
-    return false;
-  },
-
-  openFeedbackForm: function () {
-    window.mirrorGateConfig().then(function (config) {
-      document.location.href = config.backOfficeUrl + 'index.html#/feedback';
-    });
-    return false;
-  },
-
-  openBackOffice: function (boardId) {
-    window.mirrorGateConfig().then(function (config) {
-      document.location.href = config.backOfficeUrl + (boardId ?
-        'index.html#/edit/' + encodeURIComponent(boardId) + "?backToDashboard=true" :
-        'index.html#/list');
-    });
-    return false;
-  },
-
-  browserSupportsShadowDOM : function () {
-    return window.supportsShadowDOM;
-  },
-
-  compareVersions: function (version1, version2, regExp) {
-
-    var v1parts = regExp.exec(version1) || [];
-    var v2parts = regExp.exec(version2) || [];
-
-    for (var i = 1; i < Math.max(v1parts.length, v2parts.length); ++i) {
-      var part1 = parseInt(v1parts[i]) || 0;
-      var part2 = parseInt(v2parts[i]) || 0;
-
-      if (part1 !== part2) {
-        return part1 - part2;
+  return {
+    getDashboardId: function() {
+      var dashboard = document.location.hash;
+      if (dashboard && dashboard.length > 1) {
+        dashboard = dashboard.substring(1);
+      } else {
+        var searchParams = new URLSearchParams(window.location.search);
+        dashboard = searchParams.get('board');
       }
+      return dashboard || '';
+    },
+
+    getSkin: function () {
+      return this._skin || 'classic';
+    },
+
+    initSkin: function () {
+      var self = this;
+      return new Promise(function (resolve) {
+        Service
+          .get(Service.types.dashboard, Utils.getDashboardId())
+          .addListener(function(details) {
+            details = details && JSON.parse(details);
+            self._skin = (details && details.skin) || 'classic';
+            resolve(self._skin);
+          });
+      });
+    },
+
+    getWorkingDays: function(start, end) {
+      var diff = 0;
+      start = moment(start);
+      end = moment(end);
+      if (start.isBefore(end)) {
+        end.add(-1, 'days');
+        diff = moment().isoWeekdayCalc({
+          rangeStart: start,
+          rangeEnd: end,
+          weekdays: [1, 2, 3, 4, 5],
+          exclusions: [],
+          inclusions: []
+        });
+      }
+      return diff;
+    },
+
+
+    raiseEvent: function(source, detail) {
+      source.dispatchEvent(
+          new CustomEvent('dashboard-updated', {bubbles: true, detail: detail}));
+    },
+
+    toClassName: function(s) { return (s || s === 0) && ('' + s).toLowerCase().replace(' ', '-').replace('_','-'); },
+
+    openDashboard : function(type, boardId) {
+      document.location.href = type + '.html?board=' + encodeURIComponent( boardId || Utils.getDashboardId());
+      return false;
+    },
+
+    openFeedbackForm: function () {
+      window.mirrorGateConfig().then(function (config) {
+        document.location.href = config.backOfficeUrl + 'index.html#/feedback';
+      });
+      return false;
+    },
+
+    openBackOffice: function (boardId) {
+      window.mirrorGateConfig().then(function (config) {
+        document.location.href = config.backOfficeUrl + (boardId ?
+          'index.html#/edit/' + encodeURIComponent(boardId) + "?backToDashboard=true" :
+          'index.html#/list');
+      });
+      return false;
+    },
+
+    browserSupportsShadowDOM : function () {
+      return window.supportsShadowDOM;
+    },
+
+    compareVersions: function (version1, version2, regExp) {
+
+      var v1parts = regExp.exec(version1) || [];
+      var v2parts = regExp.exec(version2) || [];
+
+      for (var i = 1; i < Math.max(v1parts.length, v2parts.length); ++i) {
+        var part1 = parseInt(v1parts[i]) || 0;
+        var part2 = parseInt(v2parts[i]) || 0;
+
+        if (part1 !== part2) {
+          return part1 - part2;
+        }
+      }
+
+      return v1parts.length - v2parts.length;
+    },
+
+    rephraseVersion: function (version, regExp) {
+      var parts = regExp.exec(version);
+      return parts && (parts.length > 1) ? 'v' + parts.slice(1).join('.') : version;
+    },
+
+    normalEstimation: function (serie, confidence) {
+      serie = serie || [];
+      var z = {'80%': 0.84,'90%':1.29, '95%': 1.65, '99%': 2.33}[confidence || '80%'];
+
+      var r = {
+          count: 0,
+          total: 0,
+          avg: 0,
+          sigma: 0,
+          estimate: 0
+      };
+
+      serie.forEach(function(value) {
+          r.count++;
+          r.total += value;
+      }, this);
+      r.avg = r.total / r.count;
+
+      serie.forEach(function(value) {
+          r.sigma += Math.pow(value - r.avg,2);
+      }, this);
+
+      r.sigma = Math.sqrt(r.sigma / (r.total - 1));
+      r.estimate = r.sigma * z + r.avg;
+      return r;
+    },
+
+    notifyFavIcon: function (color) {
+      var icon = document.querySelector('link[rel*="icon"]');
+      icon.href = 'img/favicon-' + (color || 'grey') + '.png';
+      if(breakFavIconTimeout) {
+        clearTimeout(breakFavIconTimeout);
+      }
+      breakFavIconTimeout = setTimeout(function() {
+        icon.href = 'img/favicon.png';
+        breakFavIconTimeout = undefined;
+      }, 60000);
     }
-
-    return v1parts.length - v2parts.length;
-  },
-
-  rephraseVersion: function (version, regExp) {
-    var parts = regExp.exec(version);
-    return parts && (parts.length > 1) ? 'v' + parts.slice(1).join('.') : version;
-  },
-
-  normalEstimation: function (serie, confidence) {
-    serie = serie || [];
-    var z = {'80%': 0.84,'90%':1.29, '95%': 1.65, '99%': 2.33}[confidence || '80%'];
-
-    var r = {
-        count: 0,
-        total: 0,
-        avg: 0,
-        sigma: 0,
-        estimate: 0
-    };
-
-    serie.forEach(function(value) {
-        r.count++;
-        r.total += value;
-    }, this);
-    r.avg = r.total / r.count;
-
-    serie.forEach(function(value) {
-        r.sigma += Math.pow(value - r.avg,2);
-    }, this);
-
-    r.sigma = Math.sqrt(r.sigma / (r.total - 1));
-    r.estimate = r.sigma * z + r.avg;
-    return r;
   }
 
-
-};
+})();
 
 rivets.formatters.dateFrom = function(value, now) {
   return moment(value).from(now);
