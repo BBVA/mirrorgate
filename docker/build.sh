e153499333ea7a7e74f3ea6d60c9ba22bf8640bf
Alfonso Presa <alfonso.presa@bbva.com>
2017-11-27 17:24:24 +0100

    build(): clean symlinks if present....

--- docker/build.sh
+++ docker/build.sh
@@ -7,9 +7,14 @@ mkdir temp
 
 echo "Copying resources from other modules. Note they should be generated first."
 
+
 cp ../mirrorgate-api/build/libs/*.jar temp
 cp -R ../mirrorgate-dashboard/dist temp/public
+
+rm -frd temp/public/backoffice || true
 cp -R ../mirrorgate-backoffice/dist temp/public/backoffice
+
+rm -frd temp/public/docs || true
 cp -R ../mirrorgate-docs/dist temp/public/docs
 
 docker build -t mirrorgate .
