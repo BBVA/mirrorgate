b896606e5c7220323ff9b838b258034ccdb42fc5
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-11-30 17:01:01 +0100

    fix(events): make events reach aggregated dashboards

--- mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/cron/handler/FeatureEventHandler.java
+++ mirrorgate-api/src/main/java/com/bbva/arq/devops/ae/mirrorgate/cron/handler/FeatureEventHandler.java
@@ -7,11 +7,11 @@ import com.bbva.arq.devops.ae.mirrorgate.model.EventType;
 import com.bbva.arq.devops.ae.mirrorgate.model.Feature;
 import com.bbva.arq.devops.ae.mirrorgate.service.DashboardService;
 import com.bbva.arq.devops.ae.mirrorgate.service.FeatureService;
-import com.google.common.collect.Iterables;
 import java.util.Optional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.bson.types.ObjectId;
@@ -26,24 +26,22 @@ public class FeatureEventHandler implements EventHandler {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(FeatureEventHandler.class);
 
-    private final ConnectionHandler connectionHandler;
-    private final DashboardService dashboardService;
-    private final FeatureService featureService;
+    private ConnectionHandler connectionHandler;
+    private FeatureService featureService;
+    private ProcessEventsHelper eventsHelper;
 
 
     @Autowired
-    public FeatureEventHandler(ConnectionHandler connectionHandler, DashboardService dashboardService,
-        FeatureService featureService){
+    public FeatureEventHandler(ConnectionHandler connectionHandler,
+        FeatureService featureService, ProcessEventsHelper eventsHelper){
 
         this.connectionHandler = connectionHandler;
-        this.dashboardService = dashboardService;
         this.featureService = featureService;
+        this.eventsHelper = eventsHelper;
     }
 
     @Override
     public void processEvents(List<Event> eventList, Set<String> dashboardIds) {
-
-        List<Dashboard> dashboards = dashboardService.getDashboardWithNames(new ArrayList(dashboardIds));
 
         List<ObjectId> idList = eventList.stream()
                                 .map(Event::getEventTypeCollectionId)
@@ -54,15 +52,11 @@ public class FeatureEventHandler implements EventHandler {
         } else {
             Iterable<Feature> features = featureService.getFeaturesByObjectId(idList);
 
-            dashboards.forEach(dashboard -> {
-                //check if there is a feature changed
-                Optional<Feature> featureCheck = StreamSupport.stream(features.spliterator(), false)
-                    .filter(feature -> CollectionUtils.containsAny(feature.getKeywords(), dashboard.getBoards())).findFirst();
-                if(featureCheck.isPresent()){
-                    //Send update message to dashboard
-                    connectionHandler.sendEventUpdateMessage(EventType.FEATURE, dashboard.getName());
-                }
-            });
+            Predicate<Dashboard> filterDashboards = dashboard ->
+                StreamSupport.stream(features.spliterator(), false)
+                    .anyMatch(feature -> CollectionUtils.containsAny(feature.getKeywords(), dashboard.getBoards()));
+
+            eventsHelper.processEvents(dashboardIds, filterDashboards, EventType.FEATURE);
         }
     }
 }
