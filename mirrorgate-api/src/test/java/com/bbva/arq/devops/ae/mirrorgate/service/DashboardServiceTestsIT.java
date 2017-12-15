04319118277389589dbc09728ab8d3ec0c59f991
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2017-12-15 09:27:41 +0100

    fix(Test): ignore integration test

--- mirrorgate-api/src/test/java/com/bbva/arq/devops/ae/mirrorgate/service/DashboardServiceTestsIT.java
+++ mirrorgate-api/src/test/java/com/bbva/arq/devops/ae/mirrorgate/service/DashboardServiceTestsIT.java
@@ -23,12 +23,14 @@ import com.bbva.arq.devops.ae.mirrorgate.repository.DashboardRepository;
 import java.util.Collections;
 import java.util.List;
 import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DashboardServiceTestsIT {
