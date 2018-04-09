4d8a727125d4a04ef743cc25f2f59d4b17c48661
Rodrigo Quintana <rodrigo.quintana.contractor@bbva.com>
2018-04-09 16:27:01 +0200

    feat(dragula): move components to columns

--- mirrorgate-backoffice/src/app/components/app/app.module.ts
+++ mirrorgate-backoffice/src/app/components/app/app.module.ts
@@ -1,3 +1,18 @@
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { DeleteComponent } from '../delete/delete.component';
+import { DragulaModule, DragulaService } from 'ng2-dragula';
+import { FeedbackComponent } from '../feedback/feedback.component';
+import { FormComponent } from '../form/form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpModule } from '@angular/http';
+import { ListComponent } from '../list/list.component';
+import { NgModule } from '@angular/core';
+import { rootRouterConfig } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { TagInputModule } from 'ngx-chips';
 /*
  * Copyright 2017 Banco Bilbao Vizcaya Argentaria, S.A.
  *
@@ -14,24 +29,9 @@
  * limitations under the License.
  */
 
-import { NgModule } from '@angular/core'
-import { RouterModule } from '@angular/router';
-import { rootRouterConfig } from './app.routes';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { TagInputModule } from 'ngx-chips';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ListComponent } from '../list/list.component';
-import { FormComponent } from '../form/form.component';
-import { DeleteComponent } from '../delete/delete.component';
-import { FeedbackComponent } from '../feedback/feedback.component';
 
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-import { DragulaModule, DragulaService } from 'ng2-dragula';
 
 @NgModule({
   declarations: [
@@ -60,24 +60,65 @@ export class AppModule {
 
   constructor(private dragulaService: DragulaService) {
     dragulaService.setOptions('columns', {
-      copy: true,
-      copySortSource: false,
 
-      accepts: function(el, target) {
-        return target && target.classList ? !target.classList.contains('filled') : false;
+      copySortSource: false,
+      revertOnSpill: false,
+      removeOnSpill: true,
+
+      accepts: function(el, target, source) {
+
+        var componentWeight = {
+        };
+
+        componentWeight['current-sprint'] = 2;
+        componentWeight['program-increment'] = 2;
+        componentWeight['bugs'] = 1;
+        componentWeight['scm-metrics'] = 1;
+        componentWeight['alerts'] = 2;
+        componentWeight['next-sprint'] = 1;
+        componentWeight['builds'] = 2;
+        componentWeight['buildsstats'] = 1;
+        componentWeight['markets'] = 1;
+        componentWeight['reviews'] = 1;
+        componentWeight['user-metrics'] = 1;
+        componentWeight['operations-metrics'] = 1;
+
+        if(target.classList.contains('dashboard-cols-template')){
+          var elements = target.getElementsByClassName('dashboard-cols-module');
+          var total_weight = 0;
+
+          var i;
+          for(i = 0; i < elements.length; i++){
+            var element=elements[i];
+            var id = elements[i].id;
+
+            var weight = componentWeight[id];
+            total_weight = total_weight + weight;
+
+            if(total_weight > 3){
+              return false;
+            }
+          }
+        }
+
+        return !target.classList.contains('dashboard-cols-group');
+      },
+
+      copy: function (el, source) {
+        return source.classList.contains('dashboard-cols-group');
       },
     });
 
     dragulaService.drop.subscribe((value) => {
-      console.log(`drop: ${value[0]}`);
       this.onDrop(value.slice(1));
     });
   }
-  
+
   private onDrop(args) {
+    //Calculate max weight allowed
     let [e, el] = args;
     if(el && el.classList){
-      el.classList.add("filled");
+      // el.classList.add("filled");
     }
   }
 }
