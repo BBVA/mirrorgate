d5ed75a33625e4a3ac43d9b11ee89749b61daec3
Enrique Daimiel <enrique.daimiel@bbva.com>
2018-11-18 19:25:37 +0100

    chore(docs): remove bower in favor of npm dependencies & migrate to Bootstrap 4 & add fontawesome icons

--- mirrorgate-docs/scripts/vendorize.sh
+++ mirrorgate-docs/scripts/vendorize.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
-pushd bower_components
+pushd node_modules
+
+for library in 'jquery' 'bootstrap'
+do
+    for pattern in '*.min.*' '*.eot' '*.svg' '*.ttf' '*.woff*'
+    do
+        find */dist -name $library*$pattern -exec cp --parents \{\} ../src/vendor \;
+    done
+done
+
+# Include Fortawesome dependencies
 for pattern in '*.min.*' '*.eot' '*.svg' '*.ttf' '*.woff*'
 do
-    find */dist -name $pattern -exec cp --parents \{\} ../src/vendor \;
+    find @fortawesome -name $pattern -exec cp --parents \{\} ../src/vendor \;
 done
+
 popd
