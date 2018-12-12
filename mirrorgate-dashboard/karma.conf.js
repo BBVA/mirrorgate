7ba139a3ff4e6ef5e81b9f2e670b03d860d32b3e
Enrique Daimiel <enrique.daimiel@bbva.com>
2018-12-12 13:23:30 +0100

    test(dashboard): disable unit tests in firefox temporally

--- mirrorgate-dashboard/karma.conf.js
+++ mirrorgate-dashboard/karma.conf.js
@@ -126,7 +126,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['chrome','firefox'],
+    browsers: ['chrome'],
 
 
     // Continuous Integration mode
