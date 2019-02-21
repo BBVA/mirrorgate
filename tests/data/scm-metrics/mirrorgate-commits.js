10a2cd743ff28f93e6700e1606fcef5cfdd202a8
kdaimiel <enrique.daimiel@bbva.com>
2019-02-21 16:47:39 +0100

    test(commits): improve scm commits mocks

--- tests/data/scm-metrics/mirrorgate-commits.js
+++ tests/data/scm-metrics/mirrorgate-commits.js
@@ -1,18 +1,23 @@
+var now = new Date();
+var startOfMinute = parseInt((new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).getTime());
+var startOfHour = parseInt((new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours())).getTime());
+var currentTimestamp = parseInt(now.getTime());
+
 db.getCollection('commits').insertMany([
   {
     "id" : "1234123",
     "repository": "ssh://git@fake.com:fake/repo1.git",
-    "timestamp" : 1518009865,
+    "timestamp" : startOfHour,
     "branches" : {
-      "refs/remotes/origin/master": 1519231554
+      "refs/remotes/origin/master": currentTimestamp
     }
   },
   {
     "id" : "1234124",
     "repository": "ssh://git@fake.com:fake/repo1.git",
-    "timestamp" : 1518009865,
+    "timestamp" : startOfHour,
     "branches" : {
-      "refs/remotes/origin/master": 1519231554
+      "refs/remotes/origin/master": currentTimestamp
     }
   }
 ]);
