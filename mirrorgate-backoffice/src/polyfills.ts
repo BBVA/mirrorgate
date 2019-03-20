d67c1c5f35d124c969f3f6871ce8672e1d88fd81
kdaimiel <enrique.daimiel@bbva.com>
2019-03-20 12:46:00 +0100

    chore(backoffice): upgrade npm dependencies & migrate polyfills to core-js@3

--- mirrorgate-backoffice/src/polyfills.ts
+++ mirrorgate-backoffice/src/polyfills.ts
@@ -18,28 +18,28 @@
 
 import 'ie-shim'; // Internet Explorer 9 support
 
-// import 'core-js/es6';
-// Added parts of es6 which are necessary for your project or your browser support requirements.
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/set';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/weak-set';
-import 'core-js/es6/typed';
-import 'core-js/es6/reflect';
-// see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
-// import 'core-js/es6/promise';
+// import 'core-js/es';
+// Added parts of es which are necessary for your project or your browser support requirements.
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'core-js/es/weak-map';
+import 'core-js/es/weak-set';
+import 'core-js/es/typed-array';
+import 'core-js/es/reflect';
+import 'core-js/es/promise';
 
-import 'core-js/es7/reflect';
+import 'core-js/proposals/reflect-metadata';
+
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
