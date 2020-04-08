2dda0aa263462c3825bb5af7a548b60e79c664de
Enrique Daimiel <enrique.daimiel@bbva.com>
2020-04-08 16:35:25 +0200

    test(backoffice): refactor test scripts and use ChromeHeadless as a default browser for testing

--- mirrorgate-backoffice/karma.conf.js
+++ mirrorgate-backoffice/karma.conf.js
@@ -16,38 +16,95 @@
 
 module.exports = function (config) {
   config.set({
+
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+
+
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-webdriver-launcher'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+
+
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/mirrorgate-backoffice'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'kjhtml'],
+
+
+    // karma server port
     port: 9876,
+
+    // karma server hostname
+    hostname: process.env.HOSTNAME || 'localhost',
+
+
+    // enable / disable colors in the output (reporters and logs)
     colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
+
+
+    // custom launchers for WebDriver and ChromeHeadless
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox'
-        ]
+      'chrome': {
+        base: 'WebDriver',
+        config: {
+          port: process.env.SELENIUM_PORT_CHROME || 4444,
+          hostname: process.env.SELENIUM_HOST_CHROME || 'localhost'
+        },
+        browserName: 'chrome'
       },
+      'firefox': {
+        base: 'WebDriver',
+        config: {
+          port: process.env.SELENIUM_PORT_FIREFOX || 4444,
+          hostname: process.env.SELENIUM_HOST_FIREFOX || 'localhost'
+        },
+        browserName: 'firefox'
+      }
     },
-    browsers: ['Chrome'],
-    singleRun: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['ChromeHeadless'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+
     restartOnFileChange: true
   });
 };
