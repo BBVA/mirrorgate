ab95d92828b81f59b200db022da98dd059bb4297
Enrique Daimiel <enrique.daimiel@bbva.com>
2020-04-14 17:30:59 +0200

    refactor(e2e-tests): remove unusful protractor config properties

--- tests/e2e/protractor.conf.js
+++ tests/e2e/protractor.conf.js
@@ -4,12 +4,18 @@ var appHost = process.env.APP_HOST || 'localhost';
 var appPort = process.env.APP_PORT || '8080';
 
 var config = {
-    directConnect: false,
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
-      'browserName': 'chrome'
-    },
+        firefox: {
+            browserName: 'firefox',
+            name: 'MirrorGate Firefox'
+        },
+        chrome: {
+            browserName: 'chrome',
+            name: 'MirrorGate Chrome'
+        }
+    }[process.env.BROWSER || 'chrome'],
 
     // Framework to use. Jasmine 2 is recommended.
     framework: 'jasmine2',
@@ -20,29 +26,11 @@ var config = {
     // protractor is called.
     specs: ['specs/**/*.js'],
 
-    // Options to be passed to Jasmine.
-    jasmineNodeOpts: {
-        defaultTimeoutInterval: 30000
-    },
-
     seleniumAddress: 'http://'+seleniumHost+':' + seleniumPort + '/wd/hub',
 
     onPrepare: function(){
-        browser.manage().timeouts().implicitlyWait(0);
         browser.waitForAngularEnabled(false);
     },
 };
-
-//They share and modify the DB... cant be parallelized
-config.capabilities= {
-    firefox: {
-        'browserName': 'firefox',
-        name: 'MirrorGate Firefox',
-    },
-    chrome: {
-        'browserName': 'chrome',
-        name: 'MirrorGate Chrome'
-    }
-}[process.env.BROWSER || 'chrome'];
 
 exports.config = config;
